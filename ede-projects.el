7727571ecaa64c69e9cd02a15c26c180e20279d0
张志强 <zhangzhiqiang@ganji.com>
2013-06-17 23:11:29 +0800

    ede projects

---
+++ ede-projects.el
@@ -0,0 +1,3 @@
+;; EDE project cache file.
+;; This contains a list of projects you have visited.
+;; check detail on https://www.gnu.org/software/emacs/manual/html_node/ede/ede_002dcpp_002droot.html
